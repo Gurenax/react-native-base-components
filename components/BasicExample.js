57f34023f8d3b9b7b75bef090147274abd9616dc
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-15 13:57:24 +1100

    Add BasicExample component

---
+++ components/BasicExample.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Button, Text } from 'native-base'
+
+const BasicExample = () => (
+  <Container>
+    <Button>
+      <Text>Button</Text>
+    </Button>
+  </Container>
+)
+
+export default BasicExample
